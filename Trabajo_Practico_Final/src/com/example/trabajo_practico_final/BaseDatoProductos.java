040c7e8c894d25036198bfa9dff8169e736d2a34
Gabriel <josssmedn@hotmail.com>
2016-09-03 19:40:01 -0300

    borrando comentario inecesario en clase base de datos.

--- Trabajo_Practico_Final/src/com/example/trabajo_practico_final/BaseDatoProductos.java
+++ Trabajo_Practico_Final/src/com/example/trabajo_practico_final/BaseDatoProductos.java
@@ -10,7 +10,6 @@ public class BaseDatoProductos extends SQLiteOpenHelper {
 	public BaseDatoProductos(Context context, String name,
 			CursorFactory factory, int version) {
 		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
